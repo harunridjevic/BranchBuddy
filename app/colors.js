7978e406056b78e1b41f48685e67d4f4a55a7c6f
harunridjevic <harunrid@gmail.com>
2024-11-22 19:49:17 +0100

    cleanup

--- app/colors.js
+++
@@ -1,6 +0,0 @@
-export const Colors = {
-    primary:'#84E6B7',
-    secondary:'#356C79',
-    background:'#000',
-    text:'#FFF'
-}
--- app/theme.js
+++
@@ -1,6 +0,0 @@
-export const Theme = {
-    fonts: {
-        regular: './fonts/Ubuntu-Medium.ttf',
-        bold: './fonts/Ubuntu-Bold.ttf'
-    }
-};
