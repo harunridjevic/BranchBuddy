072beab8374445c2aaae131b1f94914c58a423cc
harunridjevic <harunrid@gmail.com>
2024-12-04 08:08:24 +0100

    v0.2

--- app/colors.js
+++ app/colors.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export const Colors = {
     primary:'#84E6B7',
     secondary:'#356C79',
@@ -8,20 +7,4 @@ export const Colors = {
     blue_color: '#a0b0fc',
     blue_color_secondary: '#909ee3',
     text:'#FFF',
-=======
-export const Colors = {
-    primary:'#84E6B7',
-    secondary:'#356C79',
-    tertiary:'#5196B3',
-    background:'#000',
-    text:'#FFF',
->>>>>>> 6ce4109ac8569b5fc5d77a90253a84a1b499ee38
-=======
-export const Colors = {
-    primary:'#84E6B7',
-    secondary:'#356C79',
-    tertiary:'#5196B3',
-    background:'#000',
-    text:'#FFF',
->>>>>>> 6ce4109ac8569b5fc5d77a90253a84a1b499ee38
 }
--- app/theme.js
+++ app/theme.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 export const Theme = {
     fonts: {
         regular: './fonts/Ubuntu-Medium.ttf',
         bold: './fonts/Ubuntu-Bold.ttf'
     }
 };
-=======
-export const Theme = {
-    fonts: {
-        regular: './fonts/Ubuntu-Medium.ttf',
-        bold: './fonts/Ubuntu-Bold.ttf'
-    }
-};
->>>>>>> 6ce4109ac8569b5fc5d77a90253a84a1b499ee38
-=======
-export const Theme = {
-    fonts: {
-        regular: './fonts/Ubuntu-Medium.ttf',
-        bold: './fonts/Ubuntu-Bold.ttf'
-    }
-};
->>>>>>> 6ce4109ac8569b5fc5d77a90253a84a1b499ee38
